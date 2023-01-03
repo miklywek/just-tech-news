bcd2de67320bc4205d80951c6e4ca90373ddb494
miklywek <miklywek@gmail.com>
2023-01-03 14:13:12 +0200

    add logIn and logOut

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
+
+const apiRoutes = require("./api/");
 const homeRoutes = require("./home-routes");
-router.use("/api", apiRoutes);
+
 router.use("/", homeRoutes);
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use("/api", apiRoutes);
+
+// router.use((req, res) => {
+//   res.status(404).end();
+// });
 
 module.exports = router;
