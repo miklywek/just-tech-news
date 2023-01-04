5a2d6d457aba636783873e8418e8d09c2b6a912d
miklywek <miklywek@gmail.com>
2023-01-04 13:42:28 +0200

    add comment and upvote posibility

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
-// GET /api/users
+// get all users
 router.get("/", (req, res) => {
-  // Access our User model and run .findAll() method)
-  User.findAll({ attributes: { exclude: ["password"] } })
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -12,7 +13,6 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
@@ -24,7 +24,6 @@ router.get("/:id", (req, res) => {
         model: Post,
         attributes: ["id", "title", "post_url", "created_at"],
       },
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ["id", "comment_text", "created_at"],
@@ -54,7 +53,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// POST /api/users
 router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -62,56 +60,72 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-// POST route that creates a new user
 router.post("/login", (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
     },
-    include: [
-      {
-        model: Post,
-        attributes: ["id", "title", "post_url", "created_at"],
-      },
-      {
-        model: Post,
-        attributes: ["title"],
-        through: Vote,
-        as: "voted_posts",
-      },
-    ],
   }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
-      res.json(dbUserData);
+      res.json({ user: dbUserData, message: "You are now logged in!" });
     });
   });
 });
 
-// PUT /api/users/1
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
-    attributes: { exclude: ["password"] },
+    individualHooks: true,
     where: {
-      individualHooks: true,
       id: req.params.id,
     },
   })
     .then((dbUserData) => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: "No user found with this id" });
         return;
       }
@@ -123,7 +137,6 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
@@ -141,15 +154,6 @@ router.delete("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-router.post("/logout", (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
 });
 
 module.exports = router;
