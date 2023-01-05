7f3adf7832ad8b65866f791d11b038b24651ad82
miklywek <miklywek@gmail.com>
2023-01-05 15:38:35 +0200

    add

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const homeRoutes = require("./home-routes");
 
 const dashboardRoutes = require("./dashboard-routes.js");
 
-router.use("/", homeRoutes);
+router.use("/", homeRoutes); 
 router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
 
